7d5bc4daa2d0e7175134c17ab365ef842825f60d
sainath <sainath@ufl.edu>
2016-11-25 19:09:04 -0500

    Change in Code after this

--- DDL_Parser/Xmlreader.java
+++ DDL_Parser/Xmlreader.java
@@ -6,13 +6,47 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import java.io.PrintWriter;
+import java.lang.String;
 
+class StackImpl
+{
+  private int top;
+  private String[] SArray;
+  public StackImpl()
+  {
+    top=-1;
+    SArray=new String[100];
+    
+  }
+  public void push(String s)
+  {
+    SArray[++top]=s;
+    
+  }
+  public String pop()
+  {
+    return SArray[top--];
+    
+  }
+  public String[] ReturnParent()
+  {
+    String[] temp=new String[top];
+    for (int i=0;i<=top;i++)
+    {
+      temp[i]=Sarray[i];
+    }
+    return temp;
+  }
+  
+  
+}
 public class Xmlreader {
   
 
   public static void main(String[] args) {
-
+public StackImpl SI=new StackImpl();
     try {
+      
 
 	File file = new File("./DDL_sample.xml");
   
@@ -22,7 +56,7 @@ public class Xmlreader {
 	Document doc = dBuilder.parse(file);
 
 	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-  
+  SI.push(doc.getDocumentElement().getNodeName());
 
 	if (doc.hasChildNodes()) {
 
@@ -37,51 +71,43 @@ public class Xmlreader {
   }
 
   private static void printNote(NodeList nodeList) {
+    
+    
    
-
-    for (int count = 0; count < nodeList.getLength(); count++) {
+System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
+    //push in parent
+    SI.push(tempNode.getNodeName());
+    for (int count = 0; count < nodeList.getLength();count++) {
 
 	Node tempNode = nodeList.item(count);
 
 	// make sure it's element node.
 	if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
 
-		// get node name and value
-		System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
-		System.out.println("Node Value =" + tempNode.getTextContent());
-System.out.println("test");
     
-
-		if (tempNode.hasAttributes()) {
-
-			// get attributes names and values
-			NamedNodeMap nodeMap = tempNode.getAttributes();
-
-			for (int i = 0; i < nodeMap.getLength(); i++) {
-
-				Node node = nodeMap.item(i);
-				System.out.println("attr name : " + node.getNodeName());
-				System.out.println("attr value : " + node.getNodeValue());
-        
-
-			}
-
-		}
-
 		if (tempNode.hasChildNodes()) {
 
 			// loop again if has child nodes
 			printNote(tempNode.getChildNodes());
 
 		}
+  if(tempNode.getNodeName().equals("Id")){
+    System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");   
+		System.out.println("Node Value =" + tempNode.getTextContent());
+    
+	}
 
 		System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
-   
+   //pull out parent
+    SI.pop(tempNode.getNodeName());
 
 	}
+  
 
     }
 
   }
 
 }
+
+
