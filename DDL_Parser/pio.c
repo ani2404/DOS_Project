0cf84bf488e878b7159aa4617101c355eda3f889
sainath <sainath@ufl.edu>
2016-11-26 03:05:53 -0500

    after fixing led driver set up logic

---
+++ DDL_Parser/pio.c
@@ -0,0 +1,18 @@
+bool pread(int dev_id){
+	switch(dev_id){
+		case 7:
+		gpio_read(7,data)
+		default: assert(false);		
+		}	
+	}
+bool pwrite(){	
+	switch(){	
+		case 0:
+		gpio_write(0,data)
+		case 1:
+		gpio_write(1,data)
+		case 3:
+		gpio_write(3,data)
+		default: assert(false);
+		}
+	}
