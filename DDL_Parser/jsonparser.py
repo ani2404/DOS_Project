6cf0130d558804d9aaf9ac599337dcfc1ed7da24
kps <sainath@ufl.edu>
2016-11-28 05:51:05 -0500

    Python Script

--- DDL_Parser/jsonparser.py
+++ DDL_Parser/jsonparser.py
@@ -2,37 +2,28 @@ import sys
 import json
 
 with open('ddl.json') as json_data:
-	d = json.load(json_data)
+	d=json.load(json_data)
 	listOfDevices=[]
 	ListOfTopics=[]
-	
-	
 	
 	##print d['DDL']['Platform']
 	k=d['DDL']['Platform']
 	##print d['DDL']['Platform']
 	for i in range(0,3):
 		ListOfTopics.append([])
-		
 		listOfDevices.append(k['Device'][i]['Properties']['Name'])
-	
+		ListOfTopics[i].append(k['Device'][i]['Properties']['Name'])
+		ListOfTopics[i].append(k['Device'][i]['Properties']['Id'])
 		for j in range(0,len(k['Device'][i]['Interface']['Pin']['Topic'])):
-					   ListOfTopics[i].append(k['Device'][i]['Interface']['Pin']['Topic'][j]['Name'])
-					   print ListOfTopics[i][j]
-											  
-											  
-	#print  ListOfTopics
-					   
-					   
-				   
+				ListOfTopics[i].append(k['Device'][i]['Interface']['Pin']['Topic'][j]['Name'])
+				ListOfTopics[i].append(k['Device'][i]['Interface']['Pin']['Topic'][j]['Id'])
+				ListOfTopics[i].append(k['Device'][i]['Interface']['Pin']['Topic'][j]['Mode'])
+			    ##print ListOfTopics[i][j+1]
 	print len(k['Device'])
-##with open("./ddl.json,"r+") as f:
-
-	##orig_stdout = sys.stdout
-	#f = file('output.txt', 'w')
-	#sys.stdout = f
-
-	#print "" 
-	#sys.stdout = orig_stdout
-	
-	#print "complete"
+	orig_stdout = sys.stdout
+	f = file('output.txt', 'w')
+	sys.stdout = f
+	print ListOfTopics 
+	sys.stdout = orig_stdout
+	f.close()
+print "complete"
